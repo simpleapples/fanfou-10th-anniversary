be8526369776bea98ce75f31bafc1d3fbe7cf0fc
simpleapples <zangzhiya@gmail.com>
2017-03-13 13:58:08 +0800

    feat: 完善登录逻辑

--- views/auth.py
+++ views/auth.py
@@ -5,6 +5,7 @@ from flask import Blueprint
 from flask import render_template
 from flask import redirect
 from flask import url_for
+from flask import request
 from flask_login import login_user
 from flask_login import login_required
 from flask_login import logout_user
@@ -22,36 +23,39 @@ auth_view = Blueprint('auth', __name__)
 @auth_view.route('/auth', methods=['GET', 'POST'])
 def xauth():
     form = AuthForm()
-    if form.validate_on_submit():
-        email = form.email.data
-        password = form.password.data
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            email = form.email.data
+            password = form.password.data
 
-        try:
-            consumer = {'key': const.CONSUMER_KEY,
-                        'secret': const.CONSUMER_SECRET}
-            client = fanfou.XAuth(consumer, email, password)
-            user_info = client.request('/users/show', 'POST')
-            nickname = json.loads(user_info.read().decode('utf8'))['screen_name']
             try:
-                ff_auth = FFAuth.query.equal_to('email', email).first()
-            except LeanCloudError as err:
-                if err.code == 101:
-                    ff_auth = FFAuth()
-            token = client.oauth_token['key'].decode('utf-8')
-            secret = client.oauth_token['secret'].decode('utf-8')
-            ff_auth.set('email', email)
-            ff_auth.set('nickname', nickname)
-            ff_auth.set('token', token)
-            ff_auth.set('secret', secret)
-            ff_auth.save()
-            login_user(ff_auth, True)
-            return redirect(url_for('main.index'))
-        except LeanCloudError as _:
-            error = '写入数据库失败'
-        except urllib.error.HTTPError as err:
-            error = '认证失败, 请输入正确的用户名和密码'
+                consumer = {'key': const.CONSUMER_KEY,
+                            'secret': const.CONSUMER_SECRET}
+                client = fanfou.XAuth(consumer, email, password)
+                user_info = client.request('/users/show', 'POST')
+                nickname = json.loads(user_info.read().decode('utf8'))['screen_name']
+                try:
+                    ff_auth = FFAuth.query.equal_to('email', email).first()
+                except LeanCloudError as err:
+                    if err.code == 101:
+                        ff_auth = FFAuth()
+                token = client.oauth_token['key'].decode('utf-8')
+                secret = client.oauth_token['secret'].decode('utf-8')
+                ff_auth.set('email', email)
+                ff_auth.set('nickname', nickname)
+                ff_auth.set('token', token)
+                ff_auth.set('secret', secret)
+                ff_auth.save()
+                login_user(ff_auth, True)
+                return redirect(url_for('main.index'))
+            except LeanCloudError as _:
+                error = '写入数据库失败'
+            except urllib.error.HTTPError as err:
+                error = '认证失败, 请输入正确的用户名和密码'
+        else:
+            error = '表单非法'
     else:
-        error = '表单非法'
+        error = None
     return render_template('auth.html',
                            form=form,
                            error=error)
